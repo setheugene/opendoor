8c4e29139068abc466a4f5f140cf6b628beba8a6
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-04-13 13:22:37 -0500

    Added component layouts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Nav></Nav>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
