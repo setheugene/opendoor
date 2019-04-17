316696d752345f8cef4d18369557d88e67152ed1
JakeD <43915365+jakeD9@users.noreply.github.com>
2019-04-16 19:30:00 -0500

    models setup and server runs

---
+++ models/tenant.js
@@ -0,0 +1,41 @@
+module.exports = function(sequelize, DataTypes) {
+    var Tenant = sequelize.define("Tenant", {
+        real_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        unit_number: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        rent_amount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            default: 0
+        },
+        rent_paid: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            default: false
+        },
+        contact: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: 9
+            }
+        }
+
+    });
+
+    Tenant.associate = function(models) {
+        Tenant.hasOne(models.User, {
+            foreignKey: {
+                name: 'user_id',
+                allowNull: false
+            }
+        });
+    };
+
+    return Tenant;
+}
