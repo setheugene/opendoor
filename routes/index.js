638393de9fab4a0eeb7a0db0d719289d8b6f2334
JakeD <43915365+jakeD9@users.noreply.github.com>
2019-04-18 20:17:22 -0500

    BACK END FIREBASE ADMIN AUTH SET UP

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const path = require("path");
 const router = require("express").Router();
-const apiRoutes = require("./api");
+// const apiRoutes = require("./api");
 
 // API Routes
-router.use("/api", apiRoutes);
+// router.use("/api", apiRoutes);
+
+
+// ------------------FOR JAKE TESTING FOLDER-------------------
+// router.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, "../testing/test.html"));
+// })
+// ------------------END JAKE TESTING FOLDER-------------------
 
 
 
@@ -11,5 +18,7 @@ router.use("/api", apiRoutes);
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
+
 
 module.exports = router;
