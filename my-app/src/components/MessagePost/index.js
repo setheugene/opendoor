8b28104f999113e8f285a69dfd717611e533a4c1
setheugene <seth.eugene@icloud.com>
2019-04-23 20:56:47 -0500

    message and tenant form requests

--- my-app/src/components/MessagePost/index.js
+++ my-app/src/components/MessagePost/index.js
@@ -1,6 +1,7 @@
-
 import React, { Component } from "react";
 import "./style.css";
+
+import API from '../../utils/API';
 
 class Message extends Component {
   // Setting the component's initial state
@@ -9,6 +10,13 @@ class Message extends Component {
     post: ""
   };
 
+  handlePost = event => {
+    API.insertPost(this.state.input)
+      .then(() => {
+        // reload the page
+        console.log(this.state.input)
+      });
+  };
 
   handleInputChange = event => {
     // Getting the value and name of the input which triggered the change
@@ -28,12 +36,14 @@ class Message extends Component {
       alert("Please insert a message before clicking submit.");
     } else {
       posts.push(`${this.state.input}`);
+      this.handlePost();
     }
 
     this.setState({
-      input: ""
+      input: [posts]
     });
     console.log(posts);
+   
   };
 
   render() {
@@ -42,17 +52,37 @@ class Message extends Component {
         <form onSubmit={this.handleFormSubmit}>
           <div className="form-message-post">
             <label htmlFor="exampleInputEmail1">Name</label>
-            <input type="text" className="form-control" id="message-name" placeholder="Please Enter Your Name"></input>
+            <input
+              type="text"
+              className="form-control"
+              id="message-name"
+              placeholder="Please Enter Your Name"
+            />
           </div>
           <div className="form-message-post">
             <label htmlFor="exampleInputPassword1">Room Number</label>
-            <input type="text" className="form-control" id="login-password" placeholder="Room #"></input>
+            <input
+              type="text"
+              className="form-control"
+              id="login-password"
+              placeholder="Room #"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="exampleFormControlTextarea1">Write a Message</label>
-            <textarea value={this.state.input} onChange={this.handleInputChange} name="input" className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <textarea
+              value={this.state.input}
+              onChange={this.handleInputChange}
+     
+              name="input"
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+            />
           </div>
-          <button type="submit" value="Submit" className="btn btn-primary">Submit</button>
+          <button type="submit" value="Submit" className="btn btn-primary">
+            Submit
+          </button>
         </form>
       </div>
     );
@@ -60,4 +90,3 @@ class Message extends Component {
 }
 
 export default Message;
-
--- my-app/src/components/TenantForm/index.js
+++ my-app/src/components/TenantForm/index.js
@@ -1,18 +1,115 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
+import API from '../../utils/API';
 
-function TenantInfo() {
+//write post for /api/addtenant
 
+class TenantForm extends Component {
+  // Setting the component's initial state
+  state = {
+    real_name: "",
+    unit_number: "",
+    rent_amount: "",
+    rent_paid: false,
+    contact: "",
+    username: ""
+  };
+
+
+  handlePost = event => {
+    API.addTenant(this.state.input)
+      .then(() => {
+        // reload the page
+        console.log(this.state.input)
+      });
+  };
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+  
+    event.preventDefault();
+    if (!this.state.real_name || !this.state.unit_number || !this.state.rent_amount
+      || !this.state.contact || !this.state.username){
+        alert ("Please fill out all required fields.")
+      } else {
+        this.handlePost();
+      }
+        
+    this.setState({
+        real_name: "",
+        unit_number: "",
+        rent_amount: "",
+        rent_paid: false,
+        contact: "",
+        username: ""
+    });
+    console.log(this.state);
+  };
+
+  render() {
     return (
-        <div className="container col-sm-4">
-            <h1>Tenant Info</h1>
-            <p> Move In Date:</p>
-            <p> Rent Due Date:</p>
-            <button type="button" className="btn btn-primary">Pay Rent</button><br></br>
-            <button type="button" className="btn btn-primary">Maintenance Request</button><br></br>
-            <button type="button" className="btn btn-primary">Lease Info</button>
-    </div>
+      <div className="container">
+        <form className="form-group">
+          <input className="form-control"
+            value={this.state.real_name}
+            name="real_name"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Full Name"
+          />
+          <input className="form-control"
+            value={this.state.unit_number}
+            name="unit_number"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Unit Number"
+          />
+          <input className="form-control"
+            value={this.state.rent_amount}
+            name="rent_amount"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Rent Amount"
+          />
+          <lable>Has Paid Rent?</lable>
+                    <input className="form-control"
+            value={this.state.rent_paid}
+            name="rent_paid"
+            onChange={this.handleInputChange}
+            type="checkbox"
+            id="paid"
+    
+          />
+                    <input className="form-control"
+            value={this.state.contact}
+            name="contact"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Contact Phone Number"
+          />
+                    <input className="form-control"
+            value={this.state.username}
+            name="username"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Username (e-mail)"
+          />
+          <button onClick={this.handleFormSubmit}>Submit</button>
+        </form>
+      </div>
     );
+  }
 }
 
-export default TenantInfo;
+
+export default TenantForm;
