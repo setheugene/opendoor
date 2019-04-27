2bdb4bbb009d02354bc01afcc6e628cc4c10aba6
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-04-27 12:43:53 -0500

    Added Icons

---
+++ my-app/src/components/Card/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./style.css";
+
+function TenantHome(props) {
+    return (
+        <div className="container" id="portfolio-body">
+
+            <h1 id="h1-test">Admin Utilities</h1>
+
+            <div className="row">
+
+                <div className="col-md-6" id="img-body">
+                    Add a Tenant
+                        <a className="fas fa-address-card fa-7x" href="https://beer-me-project-2.herokuapp.com/" />
+                </div>
+
+                <div className="col-md-6" id="img-body">
+                    Big Brother is Watching
+                        <a className="fas fa-eye fa-7x" href="https://beer-me-project-2.herokuapp.com/" />
+                </div>
+
+            </div>
+            <div className="row">
+
+                <div className="col-md-6" id="img-body">
+                    Add a Message
+                        <a className="fas fa-envelope fa-7x" href="https://beer-me-project-2.herokuapp.com/" />
+                </div>
+
+                <div className="col-md-6" id="img-body">
+                    View Messages
+                        <a className="fas fa-comments fa-7x" href="https://beer-me-project-2.herokuapp.com/" />
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default TenantHome;
