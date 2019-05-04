d240013cabba3763c0d17148747f173eeeb9fb0c
JakeD <43915365+jakeD9@users.noreply.github.com>
2019-05-04 12:39:01 -0500

    CRU on tenants

--- my-app/src/components/AdminViewTenant/index.js
+++ my-app/src/components/AdminViewTenant/index.js
@@ -1,15 +1,52 @@
 import React from "react";
 import "./style.css";
 
-function ViewTenant(props) {
+export function ViewTenant({ children }) {
     return (
         <div className="container" id="tenant-view-cont">
             <h1> Your Tenants </h1>
-            <div className="row">
-            </div>
+
+            <table className="table">
+                <thead>
+                    <th>Tenant Name</th>
+                    <th>Tenant Contact</th>
+                    <th>Unit #</th>
+                    <th>Rent Paid</th>
+                    <th>Edit Tenant</th>
+                </thead>
+                <tbody>
+                    {children}
+                </tbody>
+            </table>
         </div>
 
     );
 }
 
-export default ViewTenant;
+
+export function TenantList({ tName, tContact, tUnit, tRentPaid, id, grabUpdate }) {
+    let rentCheck = "";
+    if (tRentPaid) {
+        rentCheck = "paid"
+    } else {
+        rentCheck = "not paid"
+    }
+
+    return (
+        <tr>
+            <td>{tName}</td>
+            <td>{tContact}</td>
+            <td>{tUnit}</td>
+            <td>{rentCheck}</td>
+            <td>
+                <button
+                    type="submit"
+                    value={id}
+                    onClick={grabUpdate}>
+                    Update
+                </button>
+            </td>
+        </tr>
+    );
+}
+
