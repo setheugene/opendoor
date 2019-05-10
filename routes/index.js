88d8ab30ca1b386f63dac747417527ecce792287
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-05-09 19:31:39 -0500

    heroku

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../my-app/build/index.html"));
 });
 
 
