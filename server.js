51ecdf2365b29baf5f85bf84277f4ff9be055754
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-05-09 20:23:08 -0500

    Heroku

--- server.js
+++ server.js
@@ -224,7 +224,12 @@ app.delete("/api/all/tenants", (req, res) => {
     });
 });
 
-app.use(routes);
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
 
 db.sequelize.sync(syncOptions).then(function () {
   app.listen(PORT, function () {
