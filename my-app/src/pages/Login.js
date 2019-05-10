3b19f0d9ba39b7e13a3727d9225f5d3309eccd81
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-05-09 20:43:28 -0500

    Heroku

--- my-app/src/pages/Login.js
+++ my-app/src/pages/Login.js
@@ -40,7 +40,7 @@ class LoginPage extends Component {
                                     .then((res) => {
                                         console.log(res);
                                         console.log("Verified user, redirecting...");
-                                        window.location.replace("https://evening-savannah-32872.herokuapp.com/home");
+                                        window.location.replace("/home");
                                     })
                                     .catch((err) => {
                                         throw err;
