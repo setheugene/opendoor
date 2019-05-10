d87f145edfe0478d317a15f8181a8c41c0a56c99
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-05-09 19:35:59 -0500

    Heroku

--- routes/index.js
+++ routes/index.js
@@ -19,9 +19,5 @@ router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
-});
-
 
 module.exports = router;
